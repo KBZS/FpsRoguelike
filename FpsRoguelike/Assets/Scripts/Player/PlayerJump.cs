af723279c60b6697f36042127f76f052923febb6
Artur <artur.radzivilov@stud.vilniustech.lt>
2022-03-28 19:59:40 +0300

    Created PlayerJump script

---
+++ FpsRoguelike/Assets/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GroundedCheck)), RequireComponent(typeof(Rigidbody))]
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField, Min(0)] private float _jumpHeight;
+    [SerializeField, Min(0)] private int _maxJumpsCount;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private GroundedCheck _groundedCheck;
+
+    private float _jumpForce;
+    private int _leftJumpsCount;
+
+    void Start()
+    {
+        _jumpForce = CalculateJumpForce();
+        RefreshLeftJumpsCount();
+        _groundedCheck.OnGetGrounded.AddListener(RefreshLeftJumpsCount);
+    }
+
+    void Update()
+    {
+        if (_leftJumpsCount > 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            _leftJumpsCount--;
+            _rigidbody.velocity = Vector3.zero;
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void RefreshLeftJumpsCount() => _leftJumpsCount = _maxJumpsCount;
+
+    private float CalculateJumpForce()
+        => _rigidbody.mass * Mathf.Sqrt(2 *_jumpHeight * Physics.gravity.magnitude);
+
+#if UNITY_EDITOR
+
+    private void OnValidate()
+    {
+        _jumpForce = CalculateJumpForce();
+    }
+
+#endif
+}
