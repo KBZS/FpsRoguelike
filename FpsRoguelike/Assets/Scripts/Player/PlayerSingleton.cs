64497f6fb96b59b9754c12e544e9eb6fee4ab7c9
Artur <artur.radzivilov@stud.vilniustech.lt>
2022-04-18 23:43:30 +0300

    Fixed PlayerSingleton and moved IScanerTarget to it

--- FpsRoguelike/Assets/Scripts/Player/PlayerSingleton.cs
+++ FpsRoguelike/Assets/Scripts/Player/PlayerSingleton.cs
@@ -2,7 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSingleton : MonoBehaviour
+public class PlayerSingleton : MonoBehaviour, IScanerTarget
 {
-    public Transform Instance => transform;
+    public static Transform Instance;
+
+    [SerializeField] CapsuleCollider _collider;
+
+    public Vector3 Center => base.transform.position + Vector3.up * _collider.height / 2;
+
+    public new Transform transform => base.transform;
+
+    void Awake()
+    {
+        Instance = transform;
+    }
 }
